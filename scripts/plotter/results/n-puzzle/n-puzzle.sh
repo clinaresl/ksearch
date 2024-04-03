740c8cc88350e94adc3e0f82fd9b000936a98fac
Carlos Linares López <carlos.linares@uc3m.es>
2024-04-03 13:07:31 +0200

    Full generation of LaTeX tables in the n-puzzle domain

--- scripts/plotter/results/n-puzzle/n-puzzle.sh
+++ scripts/plotter/results/n-puzzle/n-puzzle.sh
@@ -24,6 +24,15 @@ function clear_images() {
         done
 }
 
+# Clearing tables
+# -----------------------------------------------------------------------------
+function clear_tables() {
+    for VARIANT in "unit" "heavy-cost"
+    do
+        rm -f results/n-puzzle/${VARIANT}/*/*.tex
+    done
+}
+
 # Creating images
 # -----------------------------------------------------------------------------
 # Brute-force variants
@@ -116,7 +125,89 @@ function create_heuristic_images() {
     done
 }
 
+# Creating tables
+# -----------------------------------------------------------------------------
+# Brute-force variants
+function create_brute_force_tables() {
 
+        # brute-force variants are tried only in the 8-Puzzle/unit variant
+        SIZE=8
+        VARIANT="unit"
+
+        ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.*${VARIANT}*csv \
+            --y runtime \
+            --series "BELA\$_0\$:re.match('BELA0', solver)" "K\$_0\$:re.match('K0', solver)" "mDijkstra:re.match('mDijkstra', solver)" \
+            --title "Runtime (seconds) - ${SIZE}-Puzzle ${VARIANT}" \
+            --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+            --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.brute-force.runtime \
+            --table
+
+        ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.* \
+            --y mem \
+            --series "BELA\$_0\$:re.match('BELA0', solver)" "K\$_0\$:re.match('K0', solver)" "mDijkstra:re.match('mDijkstra', solver)" \
+            --title "Memory (Mbytes) - ${SIZE}-Puzzle ${VARIANT}" \
+            --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+            --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.brute-force.mem \
+            --table
+
+        ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.* \
+            --y expansions \
+            --series "BELA\$_0\$:re.match('BELA0', solver)" "K\$_0\$:re.match('K0', solver)" "mDijkstra:re.match('mDijkstra', solver)" \
+            --title "#Expansions - ${SIZE}-Puzzle ${VARIANT}" \
+            --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+            --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.brute-force.expansions \
+            --table
+
+        ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.* \
+            --y nbcentroids \
+            --series "BELA\$_0\$:re.match('BELA0', solver)" "K\$_0\$:re.match('K0', solver)" "mDijkstra:re.match('mDijkstra', solver)" \
+            --title "#Centroids - ${SIZE}-Puzzle ${VARIANT}" \
+            --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+            --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.brute-force.nbcentroids \
+            --table
+}
+
+# Heuristic variants
+function create_heuristic_tables() {
+
+    for VARIANT in "unit" "heavy-cost"
+    do
+        for SIZE in 8 15
+        do
+            ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.*${VARIANT}*csv \
+                --y runtime \
+                --series "BELA\$^*\$:re.match('BELA\*', solver)" "K\$^*\$:re.match('K\*', solver)" "mA\$^*\$:re.match('mA\*', solver)" \
+                --title "Runtime (seconds) - ${SIZE}-Puzzle ${VARIANT}" \
+                --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+                --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.heuristic.runtime \
+                --table
+
+            ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.* \
+                --y mem \
+                --series "BELA\$^*\$:re.match('BELA\*', solver)" "K\$^*\$:re.match('K\*', solver)" "mA\$^*\$:re.match('mA\*', solver)" \
+                --title "Memory (Mbytes) - ${SIZE}-Puzzle ${VARIANT}" \
+                --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+                --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.heuristic.mem \
+                --table
+
+            ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.* \
+                --y expansions \
+                --series "BELA\$^*\$:re.match('BELA\*', solver)" "K\$^*\$:re.match('K\*', solver)" "mA\$^*\$:re.match('mA\*', solver)" \
+                --title "#Expansions - ${SIZE}-Puzzle ${VARIANT}" \
+                --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+                --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.heuristic.expansions \
+                --table
+
+            ./plotter.py ky --file data/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.* \
+                --y nbcentroids \
+                --series "BELA\$^*\$:re.match('BELA\*', solver)" "K\$^*\$:re.match('K\*', solver)" "mA\$^*\$:re.match('mA\*', solver)" \
+                --title "#Centroids - ${SIZE}-Puzzle ${VARIANT}" \
+                --k "1;2;3;4;5;10;50;100;500;1000;5000;10000" \
+                --output results/n-puzzle/${VARIANT}/${SIZE}/${SIZE}puzzle.heuristic.nbcentroids \
+                --table
+        done
+    done
+}
 
 
 # Main body
@@ -126,8 +217,12 @@ while [[ $# -gt 0 ]]; do
                         create_subdirectories
                         exit 0
                         ;;
-                -x|--clear)
+                -x|--clear-images)
                         clear_images
+                        exit 0
+                        ;;
+                -x|--clear-tables)
+                        clear_tables
                         exit 0
                         ;;
                 -i|--images)
@@ -135,11 +230,18 @@ while [[ $# -gt 0 ]]; do
                         create_heuristic_images
                         exit 0
                         ;;
+                -t|--tables)
+                        create_brute_force_tables
+                        create_heuristic_tables
+                        exit 0
+                        ;;
                 -h|--help)
                         echo " Use:"
-                        echo "    -c|--create   to create the subdirectories under the results directory"
-                        echo "    -x|--clear    to clear all png and gnuplot files under the results directory"
-                        echo "    -i|--images   to create the png images under the results directory"
+                        echo "    -c|--create          to create the subdirectories under the results directory"
+                        echo "    -x|--clear-images    to clear all png and gnuplot files under the results directory"
+                        echo "    -x|--clear-tables    to clear all tex files under the results directory"
+                        echo "    -i|--images          to create the png images under the results directory"
+                        echo "    -t|--tables          to create the tex files with the data body of LaTeX tables"
                         exit 0
                         ;;
                 -*|--*)
